39131c1b516cc9afce08b254b5680fe96fd2a749
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2024-06-06 13:22:02 +0700

    feat: landingpage

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { List, ListItem, ListItemText } from '@mui/material';
-import { FaHome, FaBullseye, FaRobot, FaCalendarAlt, FaCog, FaPlusSquare } from 'react-icons/fa';
+import { FaHome, FaBullseye, FaRobot, FaCalendarAlt, FaEnvelope, FaCog, FaPlusSquare } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import '../App.css';
 const Sidebar = ({ isCollapsed }) => {
   return (
     <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
       <List>
-        <ListItem button className="sidebar-item" component={Link} to="/">
+        <ListItem button className="sidebar-item" component={Link} to="/app/dashboard">
           <FaHome />
           <ListItemText primary="Dashboard" />
         </ListItem>
-        <ListItem button className="sidebar-item" component={Link} to="/goals">
+        <ListItem button className="sidebar-item" component={Link} to="/app/goals">
           <FaBullseye />
           <ListItemText primary="Goal" />
         </ListItem>
-        <ListItem button className="sidebar-item" component={Link} to="/assistant">
+        <ListItem button className="sidebar-item" component={Link} to="/app/assistant">
           <FaRobot />
           <ListItemText primary="Assistant" />
         </ListItem>
-        <ListItem button className="sidebar-item" component={Link} to="/schedule">
+        <ListItem button className="sidebar-item" component={Link} to="/app/schedule">
           <FaCalendarAlt />
           <ListItemText primary="Schedule" />
         </ListItem>
-        <ListItem button className="sidebar-item" component={Link} to="/newgoal">
+        <ListItem button className="sidebar-item" component={Link} to="/app/newgoal">
           <FaPlusSquare />
           <ListItemText primary="New Goal" />
         </ListItem>
-        <ListItem button className="sidebar-item" component={Link} to="/setting">
+        <ListItem button className="sidebar-item" component={Link} to="/app/setting">
           <FaCog />
           <ListItemText primary="Setting" />
         </ListItem>
--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -16,10 +16,10 @@ import QuantityKPIDetailPage from '../pages/goal/QuantityKPIDetailPage';
 import WeightedKPIDetailPage from '../pages/goal/WeightedKPIDetailPage';
 import NewGoalPage from '../pages/newgoal/NewGoalPage';
 import Assistant from '../pages/assistant/Assistant';
+import LandingPage from '../pages/landingpage/LandingPage'; // Make sure to import the LandingPage component
 import '../App.css';
 
 function AppRouter() {
-
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const toggleSidebar = () => {
@@ -28,28 +28,35 @@ function AppRouter() {
 
   return (
     <Router>
-      <Navbar />
-      <div className="app-container">
-        <Sidebar isCollapsed={isCollapsed} />
-        <div className={`content ${isCollapsed ? 'collapsed' : ''}`}>
-          <FaBars className="toggle-btn" onClick={toggleSidebar} />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/tracking" element={<Tracking />} />
-            <Route path="/kpis" element={<KPI />} />
-            <Route path="/tasks" element={<Task />} />
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/goals" element={<Goal />} />
-            <Route path="/goals/:id" element={<GoalDetailPage />} />
-            <Route path="/goals/:goalId/todo/:kpiId" element={<ToDoKPIDetailPage />} />
-            <Route path="/goals/:goalId/quantity/:kpiId" element={<QuantityKPIDetailPage />} />
-            <Route path="/goals/:goalId/weighted/:kpiId" element={<WeightedKPIDetailPage />} />
-            <Route path="/newgoal" element={<NewGoalPage />} />
-            <Route path="/setting" element={<Setting />} />
-            <Route path="/assistant" element={<Assistant />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app/*" element={
+          <>
+            <Navbar />
+            <div className="app-container">
+              <Sidebar isCollapsed={isCollapsed} />
+              <div className={`content ${isCollapsed ? 'collapsed' : ''}`}>
+                <FaBars className="toggle-btn" onClick={toggleSidebar} />
+                <Routes>
+                  <Route path="dashboard" element={<Dashboard />} />
+                  <Route path="tracking" element={<Tracking />} />
+                  <Route path="kpis" element={<KPI />} />
+                  <Route path="tasks" element={<Task />} />
+                  <Route path="schedule" element={<Schedule />} />
+                  <Route path="goals" element={<Goal />} />
+                  <Route path="goals/:id" element={<GoalDetailPage />} />
+                  <Route path="goals/:goalId/todo/:kpiId" element={<ToDoKPIDetailPage />} />
+                  <Route path="goals/:goalId/quantity/:kpiId" element={<QuantityKPIDetailPage />} />
+                  <Route path="goals/:goalId/weighted/:kpiId" element={<WeightedKPIDetailPage />} />
+                  <Route path="newgoal" element={<NewGoalPage />} />
+                  <Route path="setting" element={<Setting />} />
+                  <Route path="assistant" element={<Assistant />} />
+                </Routes>
+              </div>
+            </div>
+          </>
+        } />
+      </Routes>
     </Router>
   );
 };
