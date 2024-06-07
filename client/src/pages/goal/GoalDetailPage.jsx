27f517c763d303e76707769ceac24f04f7582449
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2024-06-07 12:19:28 +0700

    fix

--- client/src/pages/goal/GoalDetailPage.jsx
+++ client/src/pages/goal/GoalDetailPage.jsx
@@ -11,13 +11,48 @@ const GoalDetailPage = () => {
         return <p>Goal not found!</p>;
     }
 
+    const getMaxWeight = (tasks) => {
+        return tasks.reduce((max, task) => task.weight > max ? task.weight : max, 0);
+    };
+
+    const renderKPI = (kpi) => {
+        switch (kpi.typeKPI) {
+            case 'To-do':
+                return (
+                    <>
+                        <p>Number of Required Tasks: {kpi.task.filter(task => task.type === 'Required').length}</p>
+                        <p>Number of Optional Tasks: {kpi.task.filter(task => task.type === 'Optional').length}</p>
+                        <p>Score: {kpi.score} / 100</p>
+                    </>
+                );
+            case 'Quantity':
+                return (
+                    <>
+                        <p>Target Quantity: {kpi.target} - {kpi.unit}s</p>
+                        <p>Duration: {kpi.duration} - Split: {kpi.split}</p>
+                        <p>Score: {kpi.score} / 100</p>
+                    </>
+                );
+            case 'Weighted':
+                return (
+                    <>
+                        <p>Number of Tasks:{kpi.task.filter(task => task.type === 'Required').length}</p>
+                        <div>Max weight : {getMaxWeight(kpi.task)}</div>
+                        <p>Score: {kpi.score} / 100</p>
+                    </>
+                );
+            default:
+                return null;
+        }
+    };
+
     return (
         <div className="goal-detail">
             <div className="nav-container">
                 <div className="nav">
                     <Link to="/app/goals/">Goals</Link>
                     <span>/</span>
-                    <Link to={`/app/goals/${goal.id}`}>{goal.title}</Link> {/* Sử dụng template string để tạo URL động */}
+                    <Link to={`/app/goals/${goal.id}`}>{goal.title}</Link>
                 </div>
             </div>
             <h2>{goal.title}</h2>
@@ -49,9 +84,7 @@ const GoalDetailPage = () => {
                         >
                             <div className="kpi-widget">
                                 <h4>{kpi.name}</h4>
-                                <p>Number of Required Tasks: {kpi.task.filter(task => task.type === 'Required').length}</p>
-                                <p>Number of Optional Tasks: {kpi.task.filter(task => task.type === 'Optional').length}</p>
-                                <p>Score: {kpi.score}%</p>
+                                {renderKPI(kpi)}
                                 <div className="kpi-chart-container">
                                     <div className="kpi-chart" style={{ width: `${kpi.score}%` }}></div>
                                 </div>
