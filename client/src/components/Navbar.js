0b0d61c900648c2e5934bbe14cf2931e07120e67
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2024-06-07 12:49:34 +0700

    fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FaBell, FaUserCircle } from 'react-icons/fa';
 import Notification from '../pages/notification/Notification'; // Adjust the import path as needed
-
+import logo from '../images/Logo.png'; // Adjust the import path as needed
 import '../App.css';
 
 const Navbar = () => {
@@ -14,6 +14,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-left">
+        <img src={logo} alt="Logo" className="navbar-logo-img" />
         <h1 className="navbar-logo">EduTrack</h1>
       </div>
       <div className="navbar-right">
@@ -22,6 +23,12 @@ const Navbar = () => {
         </div>
         <div className="navbar-item">
           <FaUserCircle className="navbar-icon" />
+        </div>
+        <div className="navbar-item">
+        </div>
+        <div className="navbar-item">
+        </div>
+        <div className="navbar-item">
         </div>
       </div>
       {showNotification && <Notification />}
